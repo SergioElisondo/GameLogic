b1618928279c0dcd9756335abdbaad2a1b7493e8
Sergio Elisondo <sergioelisondo@gmail.com>
2021-04-07 17:03:05 -0700

    this is the toggle on and off logic

---
+++ toggle on and off.js
@@ -0,0 +1,11 @@
+// toggle on and off 
+
+function toggleDropdown(){
+   document.getElementById('games-dropdown').classList.toggle("hidden");
+}
+
+// css styling
+
+// .hidden {
+//     display: none;
+// }
